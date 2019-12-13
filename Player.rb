94dbf6e9d3e0945eef01efb7bc90b4f3c91c622c
Vaniik <ivan.alvarez.enri.oficial@gmail.com>
2019-12-13 11:36:52 +0100

    Player GetCollisions

--- Player.rb
+++ Player.rb
@@ -4,23 +4,38 @@ class Player
         @x = 300
         @y = 10
         @spriteSize=1.5
+        @boxCollision=[0,0]
         @speed=2
         @image = Gosu::Image.new(window, "s_player.png", true)
     end
     def draw()
         @image.draw(@x, @y, 0, @spriteSize, @spriteSize, color = 0xff_ffffff, mode = :default)
+        @boxCollision=[@image.width,@image.height]
     end
-    def move_left()
+    def Jump()
         if @x >0
             @x -= @speed
         end
     end
-    def move_right()
-        if @x < WINDOW_WIDTH-@image.width
-            @x += @speed 
-        end
-    end
+
+    #PENDIENTE DE PRUEBA
+    
     def getCollision(x,y,sizeX,sizeY)
+        if @x + @image.width/2 < x - sizeX/2
+            return 0
+        end
+        if @x -@image.width/2> x + sizeX/2
+            return 0
+        end
+        if @y + @image.height/2 < y -sizeY / 2
+            return 0
+        end
+        if @y-@image.height/2 > y + sizeY/2
+            return 0
+        end
+        return 1
+        
+        
 
 
         
