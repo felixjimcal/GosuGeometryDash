529c11747bb8800526c8019e069291544e787589
Vaniik <ivan.alvarez.enri.oficial@gmail.com>
2019-12-13 12:02:37 +0100

    Player Gancho

--- Player.rb
+++ Player.rb
@@ -6,17 +6,36 @@ class Player
         @spriteSize=1.5
         @boxCollision=[0,0]
         @speed=2
-        @image = Gosu::Image.new(window, "s_player.png", true)
+        @image = Gosu::Image.new(window, "Minecart.png", true)
+
+
+        @RopeYSize=1
+        @Rope = Gosu::Image.new(window, "Rope.png", true)
+
+        @Gancho = Gosu::Image.new(window, "Gancho.png", true)
+
     end
     def draw()
         @image.draw(@x, @y, 0, @spriteSize, @spriteSize, color = 0xff_ffffff, mode = :default)
+        @Rope.draw(@x, @y, 0, @spriteSize, @RopeYSize, color = 0xff_ffffff, mode = :default)
+        @Rope.draw(@x, @y, 0, @spriteSize, @y+@Rope.height, color = 0xff_ffffff, mode = :default)
         @boxCollision=[@image.width,@image.height]
     end
-    def Jump()
+    def move_left()
         if @x >0
             @x -= @speed
         end
     end
+    def move_right()
+        if @x < WINDOW_WIDTH-@image.width
+            @spriteSizeY+=1
+        end
+    end
+
+    def BajarGancho()
+
+    end
+    
 
     #PENDIENTE DE PRUEBA
     
